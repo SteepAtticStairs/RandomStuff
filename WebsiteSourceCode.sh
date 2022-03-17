00eb55e16d0e55c0bc38da335fe07e6256346b87
SteepAtticStairs <lawlorcube06@gmail.com>
2022-03-17 18:40:25 -0400

    added theatticrealm to source code downloader

--- WebsiteSourceCode.sh
+++ WebsiteSourceCode.sh
@@ -9,6 +9,7 @@ largeFilesURL=https://codeload.github.com/SteepAtticStairs/LargeFiles/zip/refs/h
 updateBrewURL=https://codeload.github.com/SteepAtticStairs/updateBrew/zip/refs/heads/main
 randomShellFilesURL=https://codeload.github.com/SteepAtticStairs/RandomShellFiles/zip/refs/heads/main
 zshThemesURL=https://codeload.github.com/SteepAtticStairs/ZSH-Themes/zip/refs/heads/main
+theAtticRealmURL=https://codeload.github.com/SteepAtticStairs/TheAtticRealm/zip/refs/heads/main
 now="$(date '+%m.%d.%Y')"
 
 
@@ -18,11 +19,11 @@ mkdir $dirName
 cd $dirName || exit
 
 echo ""
-echo "Would you like to download LargeFiles as well?"
-echo "YES / NO"
-read yeno
+echo "In addition, would you like to download LargeFiles, TheAtticRealm, both, or neither?"
+echo "LARGE / ATTIC / BOTH / NO"
+read seletion
 
-if [[ $yeno == yes ]]; then
+if [[ $seletion == large ]]; then
     wget -O "steepatticstairs.github.io_${now}.zip" $myWebsiteURL
     wget -O "ytdwn_${now}.zip" $ytdwnURL
     wget -O "homebrew-ytdwn_${now}.zip" $homebrewytdwnURL
@@ -30,7 +31,24 @@ if [[ $yeno == yes ]]; then
     wget -O "updateBrew_${now}.zip" $updateBrewURL
     wget -O "RandomShellFiles_${now}.zip" $randomShellFilesURL
     wget -O "ZSH-Themes_${now}.zip" $zshThemesURL
-elif [[ $yeno == no ]]; then
+elif [[ $seletion == attic ]]; then
+    wget -O "steepatticstairs.github.io_${now}.zip" $myWebsiteURL
+    wget -O "ytdwn_${now}.zip" $ytdwnURL
+    wget -O "homebrew-ytdwn_${now}.zip" $homebrewytdwnURL
+    wget -O "TheAtticRealm_${now}.zip" $theAtticRealmURL
+    wget -O "updateBrew_${now}.zip" $updateBrewURL
+    wget -O "RandomShellFiles_${now}.zip" $randomShellFilesURL
+    wget -O "ZSH-Themes_${now}.zip" $zshThemesURL
+elif [[ $seletion == both ]]; then
+    wget -O "steepatticstairs.github.io_${now}.zip" $myWebsiteURL
+    wget -O "ytdwn_${now}.zip" $ytdwnURL
+    wget -O "homebrew-ytdwn_${now}.zip" $homebrewytdwnURL
+    wget -O "LargeFiles_${now}.zip" $largeFilesURL
+    wget -O "TheAtticRealm_${now}.zip" $theAtticRealmURL
+    wget -O "updateBrew_${now}.zip" $updateBrewURL
+    wget -O "RandomShellFiles_${now}.zip" $randomShellFilesURL
+    wget -O "ZSH-Themes_${now}.zip" $zshThemesURL
+elif [[ $seletion == no ]]; then
     wget -O "steepatticstairs.github.io_${now}.zip" $myWebsiteURL
     wget -O "ytdwn_${now}.zip" $ytdwnURL
     wget -O "homebrew-ytdwn_${now}.zip" $homebrewytdwnURL
