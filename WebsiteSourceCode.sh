f250076cb767feedb982d30adcbea388e97c901a
SteepAtticStairs <lawlorcube06@gmail.com>
2022-03-28 23:17:34 -0400

    added hash lister in srccd-dw

--- WebsiteSourceCode.sh
+++ WebsiteSourceCode.sh
@@ -1,61 +1,75 @@
 #!/bin/bash
+
+bold=$(tput bold) # bold text
+normal=$(tput sgr0) # normal text
+black='\e[30m'
+red='\e[91m'
+green='\e[92m'
+yellow='\e[93m'
+blue='\e[38;5;27m'
+magenta='\e[35m'
+cyan='\e[36m'
+lightgray='\e[37m'
+darkgray='\e[90m'
+darkred='\e[31m'
+darkgreen='\e[32m'
+darkyellow='\e[33m'
+lightblue='\e[\e[38;5;39m'
+lightmagenta='\e[95m'
+lightcyan='\e[96m'
+white='\e[97m'
+nocolor='\e[0m'
+
 # srccd-dw
+
 shopt -s nocasematch
 
 myWebsiteURL=https://codeload.github.com/SteepAtticStairs/steepatticstairs.github.io/zip/refs/heads/main
 ytdwnURL=https://codeload.github.com/SteepAtticStairs/ytdwn/zip/refs/heads/main
 homebrewytdwnURL=https://codeload.github.com/SteepAtticStairs/homebrew-ytdwn/zip/refs/heads/main
 largeFilesURL=https://codeload.github.com/SteepAtticStairs/LargeFiles/zip/refs/heads/main
-updateBrewURL=https://codeload.github.com/SteepAtticStairs/updateBrew/zip/refs/heads/main
 randomStuffURL=https://codeload.github.com/SteepAtticStairs/RandomStuff/zip/refs/heads/main
-zshThemesURL=https://codeload.github.com/SteepAtticStairs/ZSH-Themes/zip/refs/heads/main
 theAtticRealmURL=https://codeload.github.com/SteepAtticStairs/TheAtticRealm/zip/refs/heads/main
 now="$(date '+%m.%d.%Y')"
 
 myWebsiteHash () {
     cd ~/Github/steepatticstairs.github.io
+    git fetch && git pull > /dev/null 2>&1
     fullHash=$(git log -1 --format="%H" origin/main)
     shortHash=$(echo ${fullHash:0:7})
     makeDir
 }
 ytdwnHash () {
     cd ~/Github/ytdwn
+    git fetch && git pull > /dev/null 2>&1
     fullHash=$(git log -1 --format="%H" origin/main)
     shortHash=$(echo ${fullHash:0:7})
     makeDir
 }
 homebrewytdwnHash () {
     cd ~/Github/homebrew-ytdwn
+    git fetch && git pull > /dev/null 2>&1
     fullHash=$(git log -1 --format="%H" origin/main)
     shortHash=$(echo ${fullHash:0:7})
     makeDir
 }
 largeFilesHash () {
     cd ~/Github/LargeFiles
-    fullHash=$(git log -1 --format="%H" origin/main)
-    shortHash=$(echo ${fullHash:0:7})
-    makeDir
-}
-updateBrewHash () {
-    cd ~/Github/updateBrew
+    git fetch && git pull > /dev/null 2>&1
     fullHash=$(git log -1 --format="%H" origin/main)
     shortHash=$(echo ${fullHash:0:7})
     makeDir
 }
 randomStuffHash () {
     cd ~/Github/RandomStuff
-    fullHash=$(git log -1 --format="%H" origin/main)
-    shortHash=$(echo ${fullHash:0:7})
-    makeDir
-}
-zshThemesHash () {
-    cd ~/Github/ZSH-Themes
+    git fetch && git pull > /dev/null 2>&1
     fullHash=$(git log -1 --format="%H" origin/main)
     shortHash=$(echo ${fullHash:0:7})
     makeDir
 }
 theAtticRealmHash () {
     cd ~/Github/TheAtticRealm
+    git fetch && git pull > /dev/null 2>&1
     fullHash=$(git log -1 --format="%H" origin/main)
     shortHash=$(echo ${fullHash:0:7})
     makeDir
@@ -70,6 +84,48 @@ makeDir () {
 }
 
 
+printHashes () {
+    cd ~/Github/steepatticstairs.github.io
+    git fetch && git pull > /dev/null 2>&1
+    fullHash=$(git log -1 --format="%H" origin/main)
+    shortHash=$(echo ${fullHash:0:7})
+    printf "${blue}steepatticstairs.github.io${nocolor}:${red} ${shortHash}${nocolor}\n"
+
+    cd ~/Github/ytdwn
+    git fetch && git pull > /dev/null 2>&1
+    fullHash=$(git log -1 --format="%H" origin/main)
+    shortHash=$(echo ${fullHash:0:7})
+    printf "${blue}ytdwn${nocolor}:${red} ${shortHash}${nocolor}\n"
+
+    cd ~/Github/homebrew-ytdwn
+    git fetch && git pull > /dev/null 2>&1
+    fullHash=$(git log -1 --format="%H" origin/main)
+    shortHash=$(echo ${fullHash:0:7})
+    printf "${blue}homebrew-ytdwn${nocolor}:${red} ${shortHash}${nocolor}\n"
+
+    cd ~/Github/LargeFiles
+    git fetch && git pull > /dev/null 2>&1
+    fullHash=$(git log -1 --format="%H" origin/main)
+    shortHash=$(echo ${fullHash:0:7})
+    printf "${blue}LargeFiles${nocolor}:${red} ${shortHash}${nocolor}\n"
+
+    cd ~/Github/RandomStuff
+    git fetch && git pull > /dev/null 2>&1
+    fullHash=$(git log -1 --format="%H" origin/main)
+    shortHash=$(echo ${fullHash:0:7})
+    printf "${blue}RandomStuff${nocolor}:${red} ${shortHash}${nocolor}\n"
+
+    cd ~/Github/TheAtticRealm
+    git fetch && git pull > /dev/null 2>&1
+    fullHash=$(git log -1 --format="%H" origin/main)
+    shortHash=$(echo ${fullHash:0:7})
+    printf "${blue}TheAtticRealm${nocolor}:${red} ${shortHash}${nocolor}\n"
+}
+
+
+echo ""
+printf "${green}Up-to-date hashes:${nocolor}\n"
+printHashes
 echo ""
 echo "In addition, would you like to download LargeFiles and TheAtticRealm, or neither?"
 echo "BOTH / NO"
