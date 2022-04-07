783881ea1d47ed4465aecc9715ffe21fb2b2597d
SteepAtticStairs <lawlorcube06@gmail.com>
2022-04-07 14:23:42 -0400

    cleaned up srccd-dw

--- WebsiteSourceCode.sh
+++ WebsiteSourceCode.sh
@@ -26,10 +26,8 @@ shopt -s nocasematch
 
 myWebsiteURL=https://codeload.github.com/SteepAtticStairs/steepatticstairs.github.io/zip/refs/heads/main
 ytdwnURL=https://codeload.github.com/SteepAtticStairs/ytdwn/zip/refs/heads/main
-homebrewytdwnURL=https://codeload.github.com/SteepAtticStairs/homebrew-ytdwn/zip/refs/heads/main
-largeFilesURL=https://codeload.github.com/SteepAtticStairs/LargeFiles/zip/refs/heads/main
+largeFilesURL=https://codeload.github.com/SteepAtticStairs/largefiles/zip/refs/heads/main
 randomStuffURL=https://codeload.github.com/SteepAtticStairs/RandomStuff/zip/refs/heads/main
-theAtticRealmURL=https://codeload.github.com/SteepAtticStairs/TheAtticRealm/zip/refs/heads/main
 now="$(date '+%m.%d.%Y')"
 
 myWebsiteHash () {
@@ -46,13 +44,6 @@ ytdwnHash () {
     shortHash=$(echo ${fullHash:0:7})
     makeDir
 }
-homebrewytdwnHash () {
-    cd ~/Github/homebrew-ytdwn
-    git fetch && git pull > /dev/null 2>&1
-    fullHash=$(git log -1 --format="%H" origin/main)
-    shortHash=$(echo ${fullHash:0:7})
-    makeDir
-}
 largeFilesHash () {
     cd ~/Github/LargeFiles
     git fetch && git pull > /dev/null 2>&1
@@ -62,13 +53,6 @@ largeFilesHash () {
 }
 randomStuffHash () {
     cd ~/Github/RandomStuff
-    git fetch && git pull > /dev/null 2>&1
-    fullHash=$(git log -1 --format="%H" origin/main)
-    shortHash=$(echo ${fullHash:0:7})
-    makeDir
-}
-theAtticRealmHash () {
-    cd ~/Github/TheAtticRealm
     git fetch && git pull > /dev/null 2>&1
     fullHash=$(git log -1 --format="%H" origin/main)
     shortHash=$(echo ${fullHash:0:7})
@@ -97,29 +81,17 @@ printHashes () {
     shortHash=$(echo ${fullHash:0:7})
     printf "${blue}ytdwn${nocolor}:${red} ${shortHash}${nocolor}\n"
 
-    cd ~/Github/homebrew-ytdwn
+    cd ~/Github/largefiles
     git fetch && git pull > /dev/null 2>&1
     fullHash=$(git log -1 --format="%H" origin/main)
     shortHash=$(echo ${fullHash:0:7})
-    printf "${blue}homebrew-ytdwn${nocolor}:${red} ${shortHash}${nocolor}\n"
-
-    cd ~/Github/LargeFiles
-    git fetch && git pull > /dev/null 2>&1
-    fullHash=$(git log -1 --format="%H" origin/main)
-    shortHash=$(echo ${fullHash:0:7})
-    printf "${blue}LargeFiles${nocolor}:${red} ${shortHash}${nocolor}\n"
+    printf "${blue}largefiles${nocolor}:${red} ${shortHash}${nocolor}\n"
 
     cd ~/Github/RandomStuff
     git fetch && git pull > /dev/null 2>&1
     fullHash=$(git log -1 --format="%H" origin/main)
     shortHash=$(echo ${fullHash:0:7})
     printf "${blue}RandomStuff${nocolor}:${red} ${shortHash}${nocolor}\n"
-
-    cd ~/Github/TheAtticRealm
-    git fetch && git pull > /dev/null 2>&1
-    fullHash=$(git log -1 --format="%H" origin/main)
-    shortHash=$(echo ${fullHash:0:7})
-    printf "${blue}TheAtticRealm${nocolor}:${red} ${shortHash}${nocolor}\n"
 }
 
 
@@ -127,22 +99,18 @@ echo ""
 printf "${green}Up-to-date hashes:${nocolor}\n"
 printHashes
 echo ""
-echo "In addition, would you like to download LargeFiles and TheAtticRealm, or neither?"
-echo "BOTH / NO"
+echo "In addition, would you like to download LargeFiles, or not?"
+echo "YES / NO"
 read seletion
 
-if [[ $seletion == both ]]; then
+if [[ $seletion == yes ]]; then
 
 myWebsiteHash
     wget -O "steepatticstairs.github.io_${now}_${shortHash}.zip" $myWebsiteURL
 ytdwnHash
     wget -O "ytdwn_${now}_${shortHash}.zip" $ytdwnURL
-homebrewytdwnHash
-    wget -O "homebrew-ytdwn_${now}_${shortHash}.zip" $homebrewytdwnURL
 largeFilesHash
-    wget -O "LargeFiles_${now}_${shortHash}.zip" $largeFilesURL
-theAtticRealmHash
-    wget -O "TheAtticRealm_${now}_${shortHash}.zip" $theAtticRealmURL
+    wget -O "largefiles_${now}_${shortHash}.zip" $largeFilesURL
 randomStuffHash
     wget -O "RandomStuff_${now}_${shortHash}.zip" $randomStuffURL
 
@@ -152,8 +120,6 @@ myWebsiteHash
     wget -O "steepatticstairs.github.io_${now}_${shortHash}.zip" $myWebsiteURL
 ytdwnHash
     wget -O "ytdwn_${now}_${shortHash}.zip" $ytdwnURL
-homebrewytdwnHash
-    wget -O "homebrew-ytdwn_${now}_${shortHash}.zip" $homebrewytdwnURL
 randomStuffHash
     wget -O "RandomStuff_${now}_${shortHash}.zip" $randomStuffURL
 
